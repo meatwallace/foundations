ce12a93e50f61e65bdd3e0c44df8003740b8fc4a
Geoff Whatley <me@geoffwhatley.com>
2019-06-16 10:23:47 +1000

    ci(semantic-release): specify plugin steps in semantic-release config
    closes #36

--- .releaserc.js
+++ .releaserc.js
@@ -1,33 +1,16 @@
 // @flow
 module.exports = {
   branch: 'master',
-  plugins: [
+  prepare: [
+    '@semantic-release/commit-analyzer',
+    '@semantic-release/release-notes-generator',
     '@semantic-release/changelog',
     '@semantic-release/npm',
     ['@semantic-release/git', {
       message: 'chore(release): ${nextRelease.gitTag} [skip ci]\n\n${nextRelease.notes}',
-    }],
-    '@semantic-release/github',
+    }]
   ],
-  // verifyConditions: [
-  //   '@semantic-release/npm',
-  //   '@semantic-release/git',
-  // ],
-  // prepare: [
-  //   '@semantic-release/changelog',
-  //   '@semantic-release/npm',
-  //   ['@semantic-release/git', {
-  //     message: 'chore(release): ${nextRelease.gitTag} [skip ci]\n\n${nextRelease.notes}',
-  //   }]
-  // ],
-  // publish: [
-  //   '@semantic-release/npm',
-  //   '@semantic-release/github',
-  // ],
-  // success: [
-  //   '@semantic-release/github',
-  // ],
-  // fail: [
-  //   '@semantic-release/github',
-  // ]
+  publish: [
+    '@semantic-release/npm',
+  ],
 };
